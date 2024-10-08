ae90717dfb620faa2060e8da360f238e377f0f5a
NITESH KUMAR <119055912+NITESH7KUMAR@users.noreply.github.com>
2024-10-08 12:41:43 +0530

    Update Home
    update Css of home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,6 +9,10 @@ function Home() {
       <div className='h1'>
       <h1 >Extraordinary<br/>Digital Experiences</h1>
       </div>
+      <div className='h2'>
+      <h2>We design, build and ship world-class digital products for forward-thinking brands.</h2>
+      </div>
+      
     </div>
     </>
   );
