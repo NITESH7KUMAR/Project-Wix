93a9fe4ad8b16d7ab74a58518319a26e734ddf59
NITESH KUMAR <119055912+NITESH7KUMAR@users.noreply.github.com>
2024-10-12 20:58:39 +0530

    update Expertise
    Update Expertise

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,15 +7,15 @@ function Navbarhome() {
     <header className="header">
       <nav className="navbar">
         <div className="logo">Humaan</div>
-        <ul className="nav-links">
+        <div className="nav-links">
           
           <li className='nav-links1'><a href="/Home">Home</a></li>
           <li className='nav-links1'><a href="/About">About</a></li>
           <li className='nav-links1'><a href="/Work">Work</a></li>
-          <li className='k'><Expertise/></li>
+          <li className='n'><Expertise/></li>
           <li className='nav-links1'><a href="/Contact">Contact</a></li>
          
-        </ul>
+        </div>
         <div className="logo1">Humaan</div>  
       </nav>
     </header>
