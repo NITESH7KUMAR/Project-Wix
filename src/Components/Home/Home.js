d4daac306698e78920d3110be9f3bb7381934c0f
NITESH KUMAR <119055912+NITESH7KUMAR@users.noreply.github.com>
2024-10-15 00:08:50 +0530

    Add assets
    Add image in home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,19 +1,55 @@
 import React from 'react';
 import './Home.css';
+import {
+  img1, img2, img3, img4, img5,
+  img6, img7, img8, img9, img10,
+  img11, img12, img13, img14
+} from "../assets";
 
 function Home() {
   return (
     <>
-    <div className='home'>
-
-      <div className='h1'>
-      <h1 >Extraordinary<br/>Digital Experiences</h1>
+      <div className='home'>
+        <div className='h1'>
+          <h1>Extraordinary<br />Digital Experiences</h1>
+        </div>
+        <div className='home-image'>
+          <img src={img1} alt="Digital Experience" />
+        </div>
+        <div className='h2'>
+          <h2>We design, build and ship world-class digital products for forward-thinking brands.</h2>
+        </div>
+        <div className='home-image'>
+          <img src={img3} alt="Digital Experience" />
+          <img src={img4} alt="Digital Experience" />
+          <img src={img5} alt="Digital Experience" />
+          <img src={img6} alt="Digital Experience" />
+        </div>
+        <div>
+          <h3>About Us</h3>
+          <div className='home-image'>
+            <img src={img2} alt="Digital Experience" />
+          </div>
+          <h2>Great work for great people.</h2>
+          <h5>We put people first, understanding that a well-crafted product significantly impacts the lives
+             of those who use it. By empowering users, we’re able to solve unique problems, accelerate progress 
+             and unlock potential for our clients. Our independent spirit drives our creative energy and approach 
+             to technology, allowing us to ensure quality and consistently deliver outstanding outcomes.</h5>
+        </div>
+        <div>
+          <h1>What's New</h1>
+          <div className='home-image'>
+            <img src={img7} alt="Digital Experience" />
+            <img src={img8} alt="Digital Experience" />
+            <img src={img9} alt="Digital Experience" />
+            <img src={img10} alt="Digital Experience" />
+            <img src={img11} alt="Digital Experience" />
+            <img src={img12} alt="Digital Experience" />
+            <img src={img13} alt="Digital Experience" />
+            <img src={img14} alt="Digital Experience" />
+          </div>
+        </div>
       </div>
-      <div className='h2'>
-      <h2>We design, build and ship world-class digital products for forward-thinking brands.</h2>
-      </div>
-      
-    </div>
     </>
   );
 }
---
+++ src/Components/assets/index.js
@@ -0,0 +1,15 @@
+// Export all images
+export { default as img1 } from './homeimg1.png';
+export { default as img2 } from './homeimg2.png';
+export { default as img3 } from './homeimg3.png';
+export { default as img4 } from './homeimg4.png';
+export { default as img5 } from './homeimg5.png';
+export { default as img6 } from './homeimg6.png';
+export { default as img7 } from './homeimg7.png';
+export { default as img8 } from './homeimg8.png';
+export { default as img9 } from './homeimg9.png';
+export { default as img10 } from './homeimg10.png';
+export { default as img11 } from './homeimg11.png';
+export { default as img12 } from './homeimg12.png';
+export { default as img13 } from './homeimg13.png';
+export { default as img14 } from './homeimg14.png';
