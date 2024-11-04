218a77228df232da6a2997523ff0ee5d4fdc12e5
NITESH KUMAR <119055912+NITESH7KUMAR@users.noreply.github.com>
2024-11-04 16:33:44 +0530

    Footer Complete

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,59 +1,59 @@
 import React from 'react';
 import './Footer.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Footer = () => {
+    const scrollTop = () => {
+        window.scrollTo({top:0 ,behavior:'smooth'});
+       
+    };
+
     return (
         <div className="footer">
-            <p>@ Mr. Nitesh Kumar, Arogya.com</p>
-            <p>© 2024 My Website. All rights reserved.</p>
 
             <footer>
-                <a href="*">
-                    <div className="foot-panel1" style={{ cursor: 'pointer' }}>
-                        Back to Top
+                    <div className="foot-panel1" onClick={scrollTop}>
+                       <p> Back to Top</p>
                     </div>
-                </a>
-
+           
                 <div className="foot-panel2">
                     <ul>
                         <p>Get to know Us</p>
-                        <a href="#home">Home</a>
-                        <a href="#about-us">About Us</a>
-                        <a href="#careers">Careers</a>
-                        <a href="#consult-doctors">Consult Doctors Online</a>
+                        <a href="Home">Home</a>
+                        <a href="About">About Us</a>
+                        <a href="Work">Work</a>
                     </ul>
-                    <ul>
+
+                    <ul className='contect'>
                         <p>Connect with Us</p>
-                        <a href="#facebook">Facebook</a>
-                        <a href="#twitter">Twitter</a>
-                        <a href="#instagram">Instagram</a>
-                        <a href="#linkedin">LinkedIn</a>
+                        <a href="#facebook">
+                            <i className="fab fa-facebook"></i> Facebook
+                        </a>
+                        <a href="#twitter">
+                            <i className="fab fa-twitter "></i> Twitter
+                        </a>
+                        <a href="#instagram">
+                            <i className="fab fa-instagram"></i> Instagram
+                        </a>
+                        <a href="#linkedin">
+                            <i className="fab fa-linkedin"></i> LinkedIn
+                        </a>
                     </ul>
                     <ul>
                         <p>Our Services</p>
-                        <a href="#order-medicine">Order Medicine</a>
-                        <a href="#healthcare-products">Healthcare Products</a>
-                        <a href="#lab-tests">Lab Tests</a>
-                        <a href="#collection-centre">Find Nearest Collection Centre</a>
-                    </ul>
-                    <ul>
-                        <p>Featured Categories</p>
-                        <a href="#top-products">Top Products</a>
-                        <a href="#personal-care">Personal Care</a>
-                        <a href="#healthcare-devices">Healthcare Devices</a>
-                        <a href="#health-food">Health Food and Drinks</a>
-                        <a href="#skin-care">Skin Care</a>
-                        <a href="#fitness-supplements">Fitness Supplements</a>
-                        <a href="#mother-baby-care">Mother and Baby Care</a>
-                        <a href="#sexual-wellness">Sexual Wellness</a>
+                        <a href="#order-medicine">Advertisement</a>
+                        <a href="#healthcare-products">Development</a>
+                        <a href="#lab-tests">Product Testing</a>
+                        <a href="#collection-centre">Collection Centre</a>
                     </ul>
                     <ul>
                         <p>Let Us Help You</p>
                         <a href="#your-account">Your Account</a>
-                        <a href="#returns">Returns Centre</a>
+                        <a href="#returns">Feedback</a>
                         <a href="#purchase-protection">100% Purchase Protection</a>
                         <a href="#help">Help</a>
                     </ul>
+                    {/* <p>© 2024 My Website. All rights reserved.</p> */}
                 </div>
             </footer>
 
