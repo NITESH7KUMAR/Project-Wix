c8994e88933127fe4cb5661c5b5e7ee5be7863f6
NITESH KUMAR <119055912+NITESH7KUMAR@users.noreply.github.com>
2024-11-07 00:35:00 +0530

    Update Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React,{Link} from 'react-router-dom';
 import './Navbar.css';
 import Expertise from '../Expertise/Expertise.js';
 
 function Navbarhome() {
   return (
+
     <header className="header">
+
       <nav className="navbar">
+
         <div className="logo">Humaan</div>
-        <div className="nav-links"> 
-          <li className='nav-links1'><a href="/Home">Home</a></li>
-          <li className='nav-links1'><a href="/About">About</a></li>
-          <li className='nav-links1'><a href="/Work">Work</a></li>
-          <li className='nav-linkss1 expertise-link '><Expertise/></li>
-          <li className='nav-links1'><a href="/Contact">Contact</a></li>
-        </div>
+
+        <lu className="nav-links"> 
+        
+          <li className='nav-links1'>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li className='nav-links1'>
+            <Link to="/About">About</Link>
+          </li>
+
+          <li className='nav-links1'>
+            <Link to="/Work">Work</Link>
+          </li>
+
+          <li className='nav-linkss1 expertise-link '>
+            <Expertise/>
+          </li>
+
+          <li className='nav-links1'>
+            <Link to="/Contact">Contact</Link>
+          </li>
+
+        </lu>
+
         <div className="logo1">Humaan</div>  
       </nav>
+
     </header>
+
   );
 }
 
